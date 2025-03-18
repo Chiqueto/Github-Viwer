58a084ecd79198d40c60f58d927a961ab43252e5
chiqueto <lfchiqueto@gmail.com>
2025-03-18 16:43:47 -0300

    atualização

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import api from "../services/api";
+import {
+  Container,
+  Header,
+  AvatarPerfil,
+  NamePerfil,
+  BioPerfil,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from "../styles";
 
 export default class User extends Component {
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { route } = this.props;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+  }
+
   render() {
+    const { route } = this.props;
+    const { user } = route.params;
+    const { stars } = this.state;
+
     return (
-      <View>
-        <Text>User</Text>
-      </View>
+      <Container>
+        <Header>
+          <AvatarPerfil source={{ uri: user.avatar }} />
+          <NamePerfil>{user.name}</NamePerfil>
+          <BioPerfil>{user.bio}</BioPerfil>
+        </Header>
+      </Container>
     );
   }
 }
